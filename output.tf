5e4c9de6ef815ac6767e7277f2ed7f9a04161828
Abhijith_C_N <abhijithcn28@gmail.com>
2025-01-28 15:30:28 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.bank-app.id
+  value = aws_eks_cluster.test_app.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.bank-app.id
+  value = aws_eks_node_group.test_app.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.bank-app_vpc.id
+  value = aws_vpc.test_app_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.bank-app_subnet[*].id
+  value = aws_subnet.test_app_subnet[*].id
 }
